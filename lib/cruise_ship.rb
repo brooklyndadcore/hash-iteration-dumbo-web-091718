b73227904953c9931518464339ec2732c945410d
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 17:59:04 +0000

    Old code that is not working

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -4,8 +4,12 @@
 # suite_c: "Alfred Tennyson", 
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
-# }
+}
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |k, v|
+    if k == :suite_a && v.start_with?("A")
+      return v
+    end
+  end
 end
